d9f009503c7de2edd8b128ea7935d5ae6fd0e2b1
crabz <hamzalagab.tech@gmail.com>
2026-01-31 15:16:56 +0100

    test

--- scripts/rabbitmq_users_ready.sh
+++ scripts/rabbitmq_users_ready.sh
@@ -9,6 +9,6 @@ rabbitmqctl list_vhosts | grep -q "^/fog$" || exit 1
 
 rabbitmqctl list_permissions -p /fog | grep -q "scheduler_worker" || exit 1
 
-[ -z "$MQWORKERUSER" ] || rabbitmqctl list_permissions -p /fog | grep -q "$MQWORKERUSER"
+[ -z "$MQ_WORKER_USER" ] || rabbitmqctl list_permissions -p /fog | grep -q "$MQ_WORKER_USER"
 
 echo "✅ Users ready!"
